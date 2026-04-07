55b69c2a13c6e031f3451d06bfd70b74006f5058
Sergei Polevikov <serg@psoftware.ru>
2026-04-07 21:26:46 +0300

    add listener

--- src/main/java/minecraft/psoftware/ru/TutorialPlugin.java
+++ src/main/java/minecraft/psoftware/ru/TutorialPlugin.java
@@ -3,6 +3,7 @@ package minecraft.psoftware.ru;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import minecraft.psoftware.ru.commands.CommandKit;
+import minecraft.psoftware.ru.listeners.PlayerJoinListener;
 
 /**
  * Hello world!
@@ -11,7 +12,11 @@ public class TutorialPlugin extends JavaPlugin  {
     @Override
     public void onEnable() {
         getLogger().info("onEnable is called!");
+        
         this.getCommand("kit").setExecutor(new CommandKit());
+        this.getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
+
+        System.out.println("Hehe");
     }
     @Override
     public void onDisable() {
